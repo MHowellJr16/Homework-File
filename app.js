2ea735a21fb4d324a667dd870bf8afc73f555f92
Mark <mhowelljr16@gmail.com>
2025-04-08 07:25:22 -0400

    Apr. 8__Update

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
+/** 
+ * ***** STRINGS ******
+ *  */ 
 console.log('Mark Howell')
+console.log('Mark Howell' + 'is an Elder!')
+console.log('Mark Howell'[0]) 
+console.log('Mark Howell'.length) 
+// console.log('Mark Howell'.length - 1)
+let str = 'Mark Howell worships God!'
+console.log(str)
+console.log(str[str.length - 1]) // Can only do 'length - 1' when declaring a variable
+/**
+ * ****** NUMBERS *******
+ */
+console.log(10 + 5)
+/**
+ * ****** LET & CONST *******
+ */
+let numbValue = 13
+console.log(numbValue)
+numbValue = numbValue + 2
+console.log(numbValue)
+const name = 'Mark Howell'
+console.log(name)
+let celsius = 10
+let farenheight = celsius * 1.8 + 32
+console.log(celsius)
+console.log(farenheight)
+/**
+ * ****** EQUALITY *******
+ */
+// Use Console Log
